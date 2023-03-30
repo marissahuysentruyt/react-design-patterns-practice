96ebfc945f20e316c7b65f6ccf8c75f137876ee3
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-03-30 11:48:45 -0400

    :sparkles: feat: new context for layouts
    Add more description of all layout components

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import { NumberedList } from "./NumberedList";
 import { Modal } from "./Modal";
 
 const LeftComponent = ({title}) => {
-  return <h1 style={{ backgroundColor: "blue"}}>{title}</h1>
+  return <h1 style={{ color: "#000", backgroundColor: "#f542ce" }}>{title}</h1>
 }
 
 const CenterComponent = ({title}) => {
-  return <h1 style={{ backgroundColor: "red"}}>{title}</h1>
+  return <h1 style={{ color: "#000", backgroundColor: "#32f0ed" }}>{title}</h1>
 }
 
 const RightComponent = ({title}) => {
-  return <h1 style={{ textTransform: "uppercase", border: "1px solid white"}}>{title}</h1>
+  return <h1 style={{ color: "#000", backgroundColor: "#fff" }}>{title}</h1>
 }
 
 const officeSupplies = [
@@ -68,30 +68,43 @@ const kitchenUtensils = [
 function App() {
   return (
     <>
+      <h1>Layout Components</h1>
+
+      <h2>Splitscreen</h2>
+      <p>Change your screen size, and you'll notice the KWL chart will keep its proportions. "K" has a "weight" or proportion (sort of like <code>fr</code> for grid styles) of 2; "W" has a weight of 6; "L" has a weight of 3.</p>
       <SplitScreen leftWeight="2" centerWeight="6" rightWeight="3">
         <LeftComponent title="K"></LeftComponent>
         <CenterComponent title="W"></CenterComponent>
         <RightComponent title="L"></RightComponent>
       </SplitScreen>
 
-      {/* the resource name must match what is named as the prop in the list item component */}
+      <h2>Regular Lists (2 ways)</h2>
+      <p>
+        The <code>itemResourceName</code> prop value is the singular version of the <code>items</code> object you pass to <code>RegularList</code>. (i.e. officeSupplies => officeSupply). That <code>itemResourceName</code> then has to match the prop that gets passed to either <code>ShortSupplyListItem</code> or <code>LongSupplyListItem</code>.
+      </p>
+      <h3>with <code>ShortSupplyListItem </code>:</h3>
       <RegularList
         items={officeSupplies}
         itemResourceName="officeSupply"
         itemComponent={ShortSupplyListItem}
       />
+      <h3>with <code>LongSupplyListItem </code>:</h3>
       <RegularList 
         items={kitchenUtensils}
         itemResourceName="kitchenUtensil"
         itemComponent={LongSupplyListItem}
       />
 
+      <h2>Numbered List</h2>
+      <p>This is very similar to the <code>RegularList</code> component, except that it uses the index of the items that were mapped through to create the returned numbers.</p>
       <NumberedList
         items={officeSupplies}
         itemResourceName="officeSupply"
         itemComponent={ShortSupplyListItem}
       />
 
+      <h2>Modal/Dialog</h2>
+      <p>Getting ahead a little, this is an example of an <em>uncontrolled</em> modal or dialog. All state for showing or not showing the modal is taken care of by the <code>Modal</code> component itself.</p>
       <Modal>
         <h2>You opened the modal! 🎉</h2>
         <RegularList 
